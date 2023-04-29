da830972a430896b96d1cf6c4cd3cd4976737923
luisesales <duduosales@hotmail.com>
2023-04-29 16:08:08 -0300

    correção de glitches e adição de método da linha

---
+++ Assets/Scripts/AimController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AimController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject lineTop;
+    public GameObject lineBottom;
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float calc = lineTop.transform.localScale.y - lineBottom.transform.localScale.y;
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if(mousePosition.y < calc) transform.position = new Vector3(transform.position.x, mousePosition.y * speed,transform.position.z);
+        else transform.position = new Vector3(transform.position.x, calc, transform.position.z);
+    }
+}
--- Assets/Scripts/LineTester.cs
+++ Assets/Scripts/LineTester.cs
@@ -1,21 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
-public class PlayerController : MonoBehaviour
+public class LineTester : MonoBehaviour
 {
-    public GameObject lineTop, lineBottom;
-    
-    public LayerMask fish;
+    [SerializeField] public Transform[] points;
+    [SerializeField] public LineController line;
     // Start is called before the first frame update
     void Start()
-    {        
+    {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        line.SetUpLine(points);
     }
 }
