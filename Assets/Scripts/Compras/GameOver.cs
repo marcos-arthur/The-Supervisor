33f547175eb08654f377985c27292b8bcbed68c7
Kimukirano <alencarcarlo@gmail.com>
2023-05-07 15:22:26 +0000

    Implementacao de audio e tela GameOver

---
+++ Assets/Scripts/Compras/GameOver.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public Text results;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // FMOD.Studio.EventInstance instance_Compras_BGM = FMODUnity.RuntimeManager.CreateInstance("event:/Compras/BGM");
+        FMOD.Studio.EventInstance instance_Compras_Win = FMODUnity.RuntimeManager.CreateInstance("event:/Compras/Win");
+        //instance_Compras_BGM.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        instance_Compras_Win.start();
+        FMODUnity.RuntimeManager.UnloadBank("bank:/Compras");
+        results.text = "Your Score: " + GameController_Compras.pontuacao;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
