8e77adfce811959d074972fd269ed81ecfbcb45b
Kimukirano <alencarcarlo@gmail.com>
2023-05-27 10:19:11 -0300

    Fmod projeto refatorado

--- Assets/Scripts/SimonSays/SimonSayScript.cs
+++ Assets/Scripts/SimonSays/SimonSayScript.cs
@@ -38,22 +38,10 @@ public class SimonSayScript : MonoBehaviour
 
     public float upTimeCounter;
 
-    public FMOD.Studio.EventInstance instance_Dragon_Says_BGM = FMODUnity.RuntimeManager.CreateInstance("event:/Dragon Says/BGM");
+    
     // Start is called before the first frame update
     void Start()
     {
-        FMODUnity.RuntimeManager.LoadBank("bank:/Dragon Says");
-        
-        FMOD.Studio.EventInstance instance_Dragon_Says_Fire = FMODUnity.RuntimeManager.CreateInstance("event:/Dragon Says/Fire");
-        FMOD.Studio.EventInstance instance_Dragon_Says_Ground = FMODUnity.RuntimeManager.CreateInstance("event:/Dragon Says/Ground");
-        FMOD.Studio.EventInstance instance_Dragon_Says_Ice = FMODUnity.RuntimeManager.CreateInstance("event:/Dragon Says/Ice");
-        FMOD.Studio.EventInstance instance_Dragon_Says_Thunder = FMODUnity.RuntimeManager.CreateInstance("event:/Dragon Says/Thunder");
-        FMOD.Studio.EventInstance instance_Dragon_Says_Correct = FMODUnity.RuntimeManager.CreateInstance("event:/Dragon Says/Correct");
-        FMOD.Studio.EventInstance instance_Dragon_Says_Miss = FMODUnity.RuntimeManager.CreateInstance("event:/Dragon Says/Miss");
-        FMOD.Studio.EventInstance instance_Dragon_Says_Fail = FMODUnity.RuntimeManager.CreateInstance("event:/Dragon Says/Fail");
-        FMOD.Studio.EventInstance instance_Dragon_Says_Marromeno = FMODUnity.RuntimeManager.CreateInstance("event:/Dragon Says/Marromeno");
-        FMOD.Studio.EventInstance instance_Dragon_Says_Win = FMODUnity.RuntimeManager.CreateInstance("event:/Dragon Says/Win");
-
 
         playButton.onClick.AddListener(StartGame);
 
@@ -79,6 +67,9 @@ public class SimonSayScript : MonoBehaviour
 
         endScore.SetActive(false);
 
+       
+        FMOD.Studio.EventInstance instance_Dragon_Says_BGM = FMODUnity.RuntimeManager.CreateInstance("event:/Dragon Says/BGM");
+        instance_Dragon_Says_BGM.start();
 
     }
 
@@ -211,7 +202,7 @@ public class SimonSayScript : MonoBehaviour
     public void StartGame()
 
     {
-            instance_Dragon_Says_BGM.start();
+           
 
             if(firstPlay == false){
 
