6579b8b3d41a606f3dd424ba334277133268da93
João Anisio <joaoanisiomn@hotmail.com>
2023-05-28 22:16:32 -0300

    Turn Off Button Added

---
+++ Assets/Scripts/General/TurnOff.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnOff : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Button turnOff;
+    void Start()
+    {
+        turnOff.onClick.AddListener(turnOffGame);
+    }
+
+    // Update is called once per frame
+
+    void turnOffGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+
+    }
+    void Update()
+    {
+        
+    }
+}
