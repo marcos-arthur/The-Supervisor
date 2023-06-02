b16295bc5632aca2c9b1b9a6e09e164f51986605
Kimukirano <alencarcarlo@gmail.com>
2023-06-02 05:24:11 -0300

    volume mixer funcionando parcialmente

--- Assets/Scripts/General/Audio/AudioController.cs
+++ Assets/Scripts/General/Audio/AudioController.cs
@@ -3,9 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
 using FMOD.Studio;
+using UnityEngine.UI;
 
 public class AudioController : MonoBehaviour
 {
+    [Header("Volume")]
+    [Range(0, 1)]
+
+    public float allVolume = 1;
+    [Range(0, 1)]
+
+    public float musicVolume = 1;
+    [Range(0, 1)]
+
+    public float sfxVolume = 1;
+    [Range(0, 1)]
+
+    private Bus allBus;
+
+    private Bus musicBus;
+
+    private Bus sfxBus;
+
+    public Slider sfxSlider;
+
+    public Slider musicSlider;
+
+    //public Slider allVolumeSlider;
+
     public static AudioController instance { get; private set; }
 
     private List<EventInstance> eventInstances;
@@ -24,6 +49,22 @@ public class AudioController : MonoBehaviour
         DontDestroyOnLoad(gameObject);
 
         eventInstances = new List<EventInstance>();
+
+        allBus = RuntimeManager.GetBus("bus:/");
+        musicBus = RuntimeManager.GetBus("bus:/Music");
+        sfxBus = RuntimeManager.GetBus("bus:/SFX"); 
+    }
+    //public void SetVolume(float sliderValue)
+    //{
+      //  AudioController.instance.musicVolume = sliderValue;
+    //}
+
+    public void Update()
+    {
+        
+        sfxBus.setVolume(sfxSlider.value);
+        musicBus.setVolume(musicSlider.value);
+        //allBus.setVolume(allVolumeSlider.value);
     }
 
     public void PlayOneShot(EventReference sound, Vector3 worldpos)
